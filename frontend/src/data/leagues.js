ecb86aa2568afd08d593aabba46e9f0b9520b225
shohruzmirzo0-cpu <shohruzmirzo0@gmail.com>
2026-07-07 13:34:09 +0500

    Create leagues.js

---
+++ frontend/src/data/leagues.js
@@ -0,0 +1,20 @@
+export const leagues = [
+  {
+    id: 1,
+    name: "🇪🇸 La Liga",
+    players: 0,
+    maxPlayers: 20,
+  },
+  {
+    id: 2,
+    name: "🏴 Premier League",
+    players: 0,
+    maxPlayers: 20,
+  },
+  {
+    id: 3,
+    name: "🇺🇿 1-Devizion",
+    players: 0,
+    maxPlayers: 20,
+  },
+];
